62e7a80d1dc3f0308eb763d060b3fcfef07c9bf1
moferrin <omarferrin2016@gmail.com>
2024-06-18 09:13:42 +0000

    clean

--- src/app.js
+++ src/app.js
@@ -11,7 +11,5 @@ app.set('port', process.env.PORT || 3006);
 app.use(express.json());
 
 app.use('/api/config', configsRoutes)
-//app.use('/api/clientes', clientesRoutes);
-//app.use('/api/pedidos', pedidosRoutes);
 
 export default app;
